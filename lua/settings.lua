619b248065d96c80358b3b8844cf217373b59131
Josh Surber <josh@surber.us>
2022-11-13 09:31:47 -0600

    Organize nvim settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,75 +1,84 @@
+--[[
+set fdm=expr fde=getline(v\:lnum)=~'^\\s*$'&&getline(v\:lnum+1)=~'\\S'?'<1'\:1
+]]
 local o = vim.opt
 local g = vim.g
 
 g.mapleader = ' '
 g.maplocalleader = ' '
 g.t_co = 256
-g.background = "dark"
 
 -- Netrw plugin options
--- g.netrw_banner = 0
 g.netrw_liststyle = 3
 g.netrw_browse_split = 4
--- g.netrw_altv = 1
 g.netrw_winsize = 25
+-- g.netrw_banner = 0
+-- g.netrw_altv = 1
 
---" Emmet
+-- Emmet
 g.user_emmet_mode = 'a' --" enable all function in all mode.
 g.user_emmet_install_global = 1 --" enable just for html/css
 g.user_emmet_leader_key = ','
 
---" Prettier
+-- Prettier
 g['prettier#autoformat'] = 1
 g['prettier#autoformat_require_pragma'] = 0
 g['prettier#config#use_tabs'] = false
 g['prettier#quickfix_auto_focus'] = 0
 
+-- Spaces and tabs
 o.tabstop = 4
 o.shiftwidth = 0
 o.softtabstop = -1 -- If negative, shiftwidth value is used
-o.lazyredraw= true -- Don't redraw screen while executing macros
-o.spell= true -- Spellcheck by default
-o.dictionary="/usr/share/dict/words"
--- Time out on key codes but not mappings.
--- Basically this makes terminal Vim work sanely.
-o.timeout = false
-o.swapfile = false-- How often does vim crash anyway?
-o.hidden = true      -- Allow unsaved buffers in background
--- New vsplit will be to right of current one
+o.cindent = true
+
+-- Spellcheck
+o.spell =  true -- Spellcheck by default
+o.dictionary = "/usr/share/dict/words"
+
+-- Wildmode ie completion
+o.wildmode = "list:longest,full" -- Expand as much as possible, then show the list
+o.wildignorecase = true -- Case insensitive completion
+
+-- Cursors and scrolling
+o.cursorline = true  -- Highlight current line
+o.scrolloff = 3
+o.sidescrolloff = 3
+
+-- Line numbers
+o.number = true
+o.relativenumber = true -- Current line actual line number, other relative to cursor
+
+-- Wrapping
+o.wrap = true -- Wrap long lines, but not in the middle of a word, and maintain indent level
+o.linebreak = true
+o.breakindent = true
+o.showbreak = "↪" -- Indicator at the start of wrapped lines
+
+-- Searching
+o.ignorecase = true -- Case insensitive searching...
+o.smartcase = true -- ...unless search includes uppercase letters...
+o.infercase = true -- ...including during insert completion
+o.showmatch = true -- Highlight matching brackets as you type
+
+-- Windows
 o.splitbelow = true
 o.splitright = true
+
+-- Misc
+o.keywordprg = ':help' -- 'K' searches vim docs
+o.lazyredraw = true -- Don't redraw screen while executing macros
+o.timeout = false -- Time out on key codes but not mappings. Basically this makes terminal Vim work sanely.
+o.swapfile = false-- How often does vim crash anyway?
 --o.path+=** -- Allows for fuzzy-like :find-ing
--- Expand as much as possible, then show the list
-o.wildmode="list:longest,full"
--- Case insensitive completion
-o.wildignorecase = true
+-- o.clipboard = 'unnamedplus' Makes neovim and host OS clipboard play nicely with each other
+-- After using it, I prefer the mappings I have set up to <leader>y/p
+o.confirm = true -- Instead of failing a command because of unsaved changes, raise a dialogue asking if you wish to save changed files.
+
+
 --[[
 o.wildignore+=*.tags,tags                    -- ignore tags files
 o.wildignore+=..git                          -- Version control
 o.wildignore+=*.jpg,*.bmp,*.gif,*.png,*.jpeg -- binary images
 o.wildignore+=*.sw?,.bak                     -- Vim swap files
 --]]
-o.scrolloff=3
-o.sidescrolloff=3
-o.showbreak="↪" -- Indicator at the start of wrapped lines
-o.cursorline = true  -- Highlight current line
-o.number = true
-o.relativenumber = true -- Current line actual line number, other relative to cursor
--- Wrap long lines, but not in the middle of a word, and maintain indent level
-o.wrap = true
-o.linebreak = true
-o.breakindent = true
--- Case insensitive searching, unless search includes uppercase letters, including during insert completion
-o.ignorecase = true
-o.smartcase = true
-o.infercase = true
--- Highlight matching brackets as you type
-o.showmatch = true
-o.cindent = true
--- Makes neovim and host OS clipboard play nicely with each other
--- o.clipboard = 'unnamedplus'
--- Instead of failing a command because of unsaved changes, instead raise a
--- dialogue asking if you wish to save changed files.
-o.confirm = true
-
-
