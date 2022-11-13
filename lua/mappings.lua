9092577ce268bde0217f3990289b260aac61b5b6
Josh Surber <josh@surber.us>
2022-11-13 09:07:35 -0600

    Organize mappings a little better in nvim

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,14 +1,24 @@
+--[[
+set fdm=expr fde=getline(v\:lnum)=~'^\\s*$'&&getline(v\:lnum+1)=~'\\S'?'<1'\:1
+]]
+
+-- Convenience functions for setting mappings
 local function map(mode, shortcut, command) vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true }) end
-local function nmap(shortcut, command) map('n', shortcut, command) end
+local function cmap(shortcut, command) map('c', shortcut, command) end
 local function imap(shortcut, command) map('i', shortcut, command) end
+local function nmap(shortcut, command) map('n', shortcut, command) end
 local function tmap(shortcut, command) map('t', shortcut, command) end
 local function vmap(shortcut, command) map('v', shortcut, command) end
 
-nmap('Y', 'yy')
---" Easy access to edit vimrc
+-- You can have my Y when you pry it from my cold, dead hands!
+vim.api.nvim_del_keymap('', 'Y')
+
+-- Easy access to edit init.lua
 nmap("<leader>ve", ":edit $MYVIMRC<cr>")
 nmap("<leader>vs", ":source $MYVIMRC<cr>")
--- Control+HJKL navigates windows
+nmap("<leader>vp", ":PackerSync<cr>")
+
+-- Window nav and resizing
 nmap("<C-h>", "<C-w>h")
 nmap("<C-j>", "<C-w>j")
 nmap("<C-k>", "<C-w>k")
@@ -17,57 +27,68 @@ nmap("<C-Up>", ":resize +2<CR>")
 nmap("<C-Down>", ":resize -2<CR>")
 nmap("<C-Left>", ":vertical resize +2<CR>")
 nmap("<C-Right>", ":vertical resize -2<CR>")
--- expand %% to current directory in command-line mode
-map("c", "%%", "<C-R>=expand('%:h').'/'<cr>")
--- leader-o/O inserts blank line below/above
-nmap('<leader>o', 'o<ESC>')
-nmap('<leader>O', 'O<ESC>')
 
-nmap("<leader>p", ":<C-u>FZF<CR>")
---" Lines wrap. Deal with it...
+-- Command line conveniences
+cmap("%%", "<C-R>=expand('%:h').'/'<cr>") -- expand %% to current directory in command-line mode
+cmap("w!!", "w !sudo tee % >/dev/null") -- No write permission? Fuck you, do it anyway!")
+
+-- leader-o/O inserts blank line below/above
+nmap('<leader>o', 'o<ESC>cc<ESC>')
+nmap('<leader>O', 'O<ESC>cc<ESC>')
+
+-- Lines wrap. Deal with it...
 nmap("j", "gj")
 nmap("k", "gk")
 map("", "<C-z>", ":set wrap!<CR>:set wrap?<CR>")
---" Folding
+
+--  Folding
 nmap("<leader><leader>", "za")
 vmap("<leader><leader>", "za")
---nmap("/", "/\\v")
---vmap("/", "/\\v")
 
--- Fix * (Keep the cursor position, don't move to next match)
-nmap('*', '*N')
---
--- -- Fix n and N. Keeping cursor in center
-nmap('n', 'nzz')
-nmap('N', 'Nzz')
+-- Searching nicities
+nmap('*', '*N') -- Fix * (Keep the cursor position, don't move to next match)
+nmap('n', 'nzz') -- Fix n and N to...
+nmap('N', 'Nzz') -- ...keep the cursor in center
+nmap("<leader>\\", "<cmd>noh<cr>") --  remove search highlighting
+nmap("<esc>", "<cmd>noh<cr>") --  remove search highlighting
 
-nmap("<tab>", "%")
---" remove search highlighting
-nmap("<leader>\\", "<cmd>noh<cr>")
-nmap("<esc>", "<cmd>noh<cr>")
---" Move to end of pasted text; easily select same
-vmap("y", "y`]")
-vmap("p", "p`]")
-nmap("p", "p`]")
-nmap("gV", "`[v`]")
-
-nmap('<C-e>', ':Lexplore<CR>')
-
---" Fix linewise visual selection of various text objects
+--  Fix oddities with visual selections
+--  Fix linewise visual selection of various text objects
 nmap("VV", "V")
 nmap("Vit", "vitVkoj")
 nmap("Vat", "vatV")
 nmap("Vab", "vabV")
 nmap("VaB", "vaBV")
+--  Move to end of pasted text; easily select same
+vmap("y", "y`]")
+vmap("p", "p`]")
+nmap("p", "p`]")
+nmap("gV", "`[v`]")
+-- don't leave visual selection mode after changing indentation
+vmap('>', '>gv')
+vmap('<', '<gv')
+vmap('=', '=gv')
 
+-- Easier access to system clipboard
+nmap('<leader>p', '"+p')
+nmap('<leader>y', '"+y')
+vmap('<leader>y', '"+y')
+nmap('<leader>P', '"+P')
+nmap('<leader>Y', '"+Y')
+vmap('<leader>Y', '"+Y')
+
+-- Change modes easier
 imap("jk", "<esc>")
-vmap("jk", "<esc>")
+-- vmap("jk", "<esc>")
+tmap("<Esc>", "<C-\\><C-n>")
+
+-- Save and quit easier
 nmap("<leader>w", "<cmd>w<cr>")
 nmap("<leader>W", "<cmd>wall<cr>")
 nmap("<leader>qq", "<cmd>q<cr>")
 nmap("<leader>Q", "<cmd>qall<cr>")
 nmap("QQ", ":q<cr>")
-map("c", "w!!", "w !sudo tee % >/dev/null") -- No write permission? Fuck you, do it anyway!")
+
 -- Move line up and down in NORMAL and VISUAL modes
 -- -- Reference: https://vim.fandom.com/wiki/Moving_lines_up_or_down
 nmap('<C-j>', '<CMD>move .+1<CR>==')
@@ -76,34 +97,14 @@ vmap('<C-j>', ":move '>+1<CR>gv=gv")
 vmap('<C-k>', ":move '<-2<CR>gv=gv")
 imap('<C-j>', '<Esc>:m .+1<CR>==gi')
 imap('<C-k>', '<Esc>:m .-2<CR>==gi')
--- don't leave visual selection mode after changing indentation
-vmap('>', '>gv')
-vmap('<', '<gv')
-vmap('=', '=gv')
 
--- Insert new line in normal mode
-nmap('<S-CR>', 'i<CR><Esc>')
---run default macro (recorded with qq)
-nmap('Q', '@q')
+nmap('Q', '@q') --run default macro (recorded with qq)
 
-tmap("<Esc>", "<C-\\><C-n>")
---[[
-tmap("<A-h>", "<C-\\><C-N><C-w>h")
-tmap("<A-j>", "<C-\\><C-N><C-w>j")
-tmap("<A-k>", "<C-\\><C-N><C-w>k")
-tmap("<A-l>", "<C-\\><C-N><C-w>l")
-imap("<A-h>", "<C-\\><C-N><C-w>h")
-imap("<A-j>", "<C-\\><C-N><C-w>j")
-imap("<A-k>", "<C-\\><C-N><C-w>k")
-imap("<A-l>", "<C-\\><C-N><C-w>l")
-nmap("<A-h>", "<C-w>h")
-nmap("<A-j>", "<C-w>j")
-nmap("<A-k>", "<C-w>k")
-nmap("<A-l>", "<C-w>l")
-]]
-nmap('<leader>p', '"*p')
-nmap('<leader>y', '"*y')
-vmap('<leader>y', '"*y')
-nmap('<leader>P', '"*P')
-nmap('<leader>Y', '"*Y')
-vmap('<leader>Y', '"*Y')
+-- Simplify accessing keys chorded to my escape key
+map('!', '<A-ESC>', '~') -- Tilde is a third layer on my esc key. Fuck that
+imap('<A-\'>', '`') -- Backtick is also a function layer on esc. To hell with that shit
+
+nmap("<tab>", "%")
+
+nmap('<C-e>', ':Lexplore<CR>')
+
