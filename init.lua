ee1e861d6f5097ad3f0d27a62adff0f8eb6b021a
Josh Surber <josh@surber.us>
2022-11-19 09:35:47 -0600

    Add some `neovide(1)` config to init.lua
    If I wind up with more than 5-10 LOC for neovide I'll put them in their
    own module. I also need to move away from calling `vim.cmd` but I
    copy/pasted from neovide.io

--- init.lua
+++ init.lua
@@ -13,3 +13,11 @@ vim.o.background = "dark" -- or "light" for light mode
 vim.cmd [[silent! colorscheme evening]]
 vim.cmd [[silent! colorscheme gruvbox]]
 -- vim.cmd [[silent! colorscheme gruvbox-baby]]
+
+-- Neovide config
+vim.o.guifont = "CascadiaCode_Nerd_Font,Cascadia_Code_PL,Hack:h10"
+vim.cmd [[
+let g:neovide_floating_blur_amount_x = 50
+let g:neovide_floating_blur_amount_y = 50
+let g:neovide_cursor_vfx_mode = "railgun"
+]]
