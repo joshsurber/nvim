910f9abe9fc3f19ca46df841786a35016118406e
Josh Surber <josh@surber.us>
2022-11-19 10:13:46 -0600

    Remove unneeded lines from init.lua

--- init.lua
+++ init.lua
@@ -3,13 +3,12 @@
 vim.g.mapleader = " "
 
 require('plugins')
---" lua require('vimplug')
 require('settings')
 require('mappings')
 require('autocmds')
 
 --" If we haven't installed gruvbox yet, use a build-in colorscheme
-vim.o.background = "dark" -- or "light" for light mode
+-- vim.o.background = "dark" -- or "light" for light mode
 vim.cmd [[silent! colorscheme evening]]
 vim.cmd [[silent! colorscheme gruvbox]]
 -- vim.cmd [[silent! colorscheme gruvbox-baby]]
