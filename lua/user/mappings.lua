a189ea3e23ce75f1c6aefdef2d68c96af12cc331
Josh Surber <josh@surber.us>
2022-12-21 05:31:19 -0600

    nvim: Add descriptions to mappings

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -20,14 +20,14 @@ map("n", "<leader>vd", ":cd ~/.config/nvim<cr>", { desc = 'Change to Neovim conf
 map("n", "<leader>vp", ":PackerSync<cr>", { desc = "Sync plugins" })
 
 -- Window nav and resizing
--- map("n", "<C-h>", "<C-w>h")
--- map("n", "<C-j>", "<C-w>j")
--- map("n", "<C-k>", "<C-w>k")
--- map("n", "<C-l>", "<C-w>l")
-map("n", "<C-Up>", ":resize +2<CR>")
-map("n", "<C-Down>", ":resize -2<CR>")
-map("n", "<C-Left>", ":vertical resize +2<CR>")
-map("n", "<C-Right>", ":vertical resize -2<CR>")
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+map("n", "<C-Up>", ":resize +2<CR>", { desc = 'Make window taller' })
+map("n", "<C-Down>", ":resize -2<CR>", { desc = 'Make window shorter' })
+map("n", "<C-Left>", ":vertical resize +2<CR>", { desc = 'Make window wider' })
+map("n", "<C-Right>", ":vertical resize -2<CR>", { desc = 'Make window narrower' })
 
 map('n', '/', '/\\v')
 
@@ -40,35 +40,36 @@ map("n", '<leader>o', 'o<ESC>cc<ESC>', { desc = 'Insert blank line below' })
 map("n", '<leader>O', 'O<ESC>cc<ESC>', { desc = 'Insert blank line above' })
 
 -- Lines wrap. Deal with it...
-map("n", "<DOWN>", "gj")
-map("n", "<UP>", "gk")
-map("", "<C-z>", ":set wrap!<CR>:set wrap?<CR>")
+map("n", "<DOWN>", "gj", { desc = "Down visual line" })
+map("n", "<UP>", "gk", { desc = "Up visual lines" })
+map("", "<C-z>", ":set wrap!<CR>:set wrap?<CR>", { desc = "Toggle word wrap" })
 
 -- Folding
 map({ 'n', 'v' }, "<leader><leader>", "za", { desc = 'Toggle fold' })
 
 -- Searching nicities
-map("n", '*', '*N') -- Fix * (Keep the cursor position, don't move to next match)
-map("n", 'n', 'nzz') -- Fix n and N to...
-map("n", 'N', 'Nzz') -- ...keep the cursor in center
+map("n", '*', '*N', { desc = 'Search word under cursor' }) -- Fix * (Keep the cursor position, don't move to next match)
+map("n", 'n', 'nzzzv', { desc = 'Go to next match' }) -- Fix n and N to...
+map("n", 'N', 'Nzzzv', { desc = 'Go to previous match' }) -- ...keep the cursor in center
+map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace word under cursor" })
 map("n", "<esc>", "<cmd>noh<cr>") --  remove search highlighting
 
 -- Fix oddities with visual selections
 -- Fix linewise visual selection of various text objects
-map("n", "VV", "V")
-map("n", "Vit", "vitVkoj")
-map("n", "Vat", "vatV")
-map("n", "Vab", "vabV")
-map("n", "VaB", "vaBV")
+map("n", "VV", "V", { desc = "Select visual lines" })
+map("n", "Vit", "vitVkoj", { desc = "Select inside a tag" })
+map("n", "Vat", "vatV", { desc = "Select around a tag" })
+map("n", "Vab", "vabV", { desc = "Select a block" })
+map("n", "VaB", "vaBV", { desc = "Select a block" })
 --  Move to end of pasted text; easily select same
-map("v", "y", "y`]")
-map("v", "p", "p`]")
-map("n", "p", "p`]")
-map("n", "gV", "`[v`]")
+map("v", "y", "y`]", { desc = "Yank selection" })
+map("v", "p", "p`]", { desc = "Replace selection with register (paste)" })
+map("n", "p", "p`]", { desc = "Put buffer contents (paste)" })
+map("n", "gV", "`[v`]", { desc = "Reselect last selection" })
 -- don't leave visual selection mode after changing indentation
-map("v", '>', '>gv')
-map("v", '<', '<gv')
-map("v", '=', '=gv')
+map("v", '>', '>gv', { desc = "Indent more" })
+map("v", '<', '<gv', { desc = "Indent less" })
+map("v", '=', '=gv', { desc = "Fix indentation" })
 
 -- Easier access to system clipboard
 map("n", '<leader>p', '"+p==', { desc = 'Paste from system clipboard' })
@@ -77,6 +78,7 @@ map("v", '<leader>y', '"+y', { desc = 'Copy to system clipboard' })
 map("n", '<leader>P', '"+P==', { desc = 'Paste from system clipboard' })
 map("n", '<leader>Y', '"+Y', { desc = 'Copy lines to system clipboard' })
 map("v", '<leader>Y', '"+Y', { desc = 'Copy lines to system clipboard' })
+map("x", "<leader>p", '"_dP', { desc = "Paste over selection without modifying registers" })
 
 -- Change modes easier
 map("i", "jk", "<esc>")
@@ -91,30 +93,34 @@ map("n", "<leader>Q", "<cmd>qall<cr>", { desc = 'Close all windows/tabs' })
 map("n", "QQ", ":q<cr>")
 -- Move line up and down in NORMAL and VISUAL modes
 -- -- Reference: https://vim.fandom.com/wiki/Moving_lines_up_or_down
-map("n", '<A-j>', '<CMD>move .+1<CR>==')
-map("n", '<A-k>', '<CMD>move .-2<CR>==')
-map("v", '<A-j>', ":move '>+1<CR>gv=gv")
-map("v", '<A-k>', ":move '<-2<CR>gv=gv")
-map("i", '<A-j>', '<Esc>:m .+1<CR>==gi')
-map("i", '<A-k>', '<Esc>:m .-2<CR>==gi')
+map("n", '<A-j>', '<CMD>move .+1<CR>==', { desc = "Move line down" })
+map("n", '<A-k>', '<CMD>move .-2<CR>==', { desc = "Move line up" })
+map("v", '<A-j>', ":move '>+1<CR>gv=gv", { desc = "Move line(s) down" })
+map("v", '<A-k>', ":move '<-2<CR>gv=gv", { desc = "Move line(s) up" })
+map("i", '<A-j>', '<Esc>:m .+1<CR>==gi', { desc = "Move line down" })
+map("i", '<A-k>', '<Esc>:m .-2<CR>==gi', { desc = "Move line up" })
+map("n", '<A-J>', 'yyp', { desc = "Duplicate line below" })
+map("n", '<A-K>', 'yyP', { desc = "Duplicate line above" })
 
 -- Simplify accessing keys chorded to my escape key
-map('!', '<A-ESC>', '~') -- Tilde is a third layer on my esc key. Fuck that
+map('', '<A-ESC>', '~') -- Tilde is a third layer on my ESC key. Fuck that
+map('!', '<A-ESC>', '~') -- Remap it in every way possible
 map("i", '<A-\'>', '`') -- Backtick is also a function layer on esc. To hell with that shit
 
-map("n", '<C-q>', '@q') --run default macro (recorded with qq)
+map("n", '<C-q>', '@q', { desc = "run default macro (from qq)" })
 
 -- map("n","<tab>", "%")
 
-map("n", '<S-h>', ':bprev<cr>')
-map("n", '[b', ':bprev<cr>')
-map("n", '<S-l>', ':bnext<cr>')
-map("n", ']b', ':bnext<cr>')
+map("n", '<S-h>', ':bprev<cr>', { desc = "Previous buffer" })
+map("n", '[b', ':bprev<cr>', { desc = "Next buffer" })
+map("n", '<S-l>', ':bnext<cr>', { desc = "Previous buffer" })
+map("n", ']b', ':bnext<cr>', { desc = "Next buffer" })
 
 map("n", '<leader>e', ':Lexplore<CR>', { desc = 'Toggle file tree' })
 -- This is for a weird bug I get with a single-line window and empty buffer
 -- Hopefully will be fixed in future nvim release. FIXME delete if fixed
 map("n", '<leader>_', ':Bdelete<cr>:resize<cr>', { desc = 'Fix window sizes' })
+
 
 -- LSP mappings
 WKRegister('<leader>l', 'LSP commands')
