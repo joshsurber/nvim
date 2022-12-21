978b0d8eff3b491a4d6e678de47fcfe31aa84f98
Josh Surber <josh@surber.us>
2022-12-21 05:15:56 -0600

    Use `vim.cmd.colorscheme()` syntax

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,7 +1,8 @@
 --" If we haven't installed gruvbox yet, use a build-in colorscheme
 -- vim.o.background = "dark" -- or "light" for light mode
-vim.cmd [[silent! colorscheme evening]]
--- vim.cmd [[silent! colorscheme gruvbox]]
--- vim.cmd [[silent! colorscheme dracula]]
--- vim.cmd [[silent! colorscheme monokai]]
-vim.cmd [[silent! colorscheme tokyonight-night]]
+vim.g.starry_italic_comments = true
+vim.cmd.colorscheme("evening")
+-- vim.cmd.colorscheme("gruvbox")
+-- vim.cmd.colorscheme("dracula")
+-- vim.cmd.colorscheme("monokai")
+vim.cmd.colorscheme("tokyonight-night")
