0b35de7f8d17c999987d57ebc6f5b69b315c8d6f
Josh Surber <josh@surber.us>
2022-12-21 05:34:19 -0600

    nvim: Organize settings.lua

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,48 +1,51 @@
 for key, value in pairs({ -- NeoVim settings
-	breakindent = true,
-	cindent = true,
-	confirm = true, -- Instead of failing a command because of unsaved changes, raise a dialogue asking if you wish to save changed files.
-	cursorline = true, -- Highlight current line
-	dictionary = "/usr/share/dict/words",
-	ignorecase = true, -- Case insensitive searching...
-	infercase = true, -- ...including during insert completion
-	keywordprg = ':help', -- 'K' searches vim docs
-	lazyredraw = true, -- Don't redraw screen while executing macros
-	linebreak = true,
-	number = true,
-	-- o.clipboard = 'unnamedplus',-- Makes neovim and host OS clipboard play nicely with each other
-	relativenumber = true, -- Current line actual line number, other relative to cursor
-	scrolloff = 3,
-	-- sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions",
-	shiftwidth = 0,
-	showbreak = "↪", -- Indicator at the start of wrapped lines
-	showmatch = true, -- Highlight matching brackets as you type
-	sidescrolloff = 3,
-	smartcase = true, -- ...unless search includes uppercase letters...
-	softtabstop = -1, -- If negative, shiftwidth value is used
-	spell = true, -- Spellcheck by default
-	splitbelow = true,
-	splitright = true,
-	swapfile = false, -- How often does vim crash anyway?
-	tabstop = 4,
-	termguicolors = true,
-	-- timeout = false, -- Time out on key codes but not mappings. Basically this makes terminal Vim work sanely.
-	timeoutlen = 500,
-	wildignorecase = true, -- Case insensitive completion
-	-- wildmode = "list:longest,full", -- Expand as much as possible, then show the list
-	wrap = true, -- Wrap long lines, but not in the middle of a word, and maintain indent level
+
+    breakindent = true, -- Indent lines when wrapping text
+    cindent = true, -- Indent using C-style rules
+    confirm = true, -- Instead of failing a command because of unsaved changes, raise a dialogue asking if you wish to save changed files.
+    cursorline = true, -- Highlight current line
+    dictionary = "/usr/share/dict/words",
+    expandtab = true, -- Use spaces instead of tabs
+    ignorecase = true, -- Case insensitive searching...
+    infercase = true, -- ...including during insert completion
+    keywordprg = ':help', -- 'K' searches vim docs
+    lazyredraw = true, -- Don't redraw screen while executing macros
+    linebreak = true, -- Break lines at `breakat` chars instead of last col
+    number = true, -- Show line numbers (with relnumber, make current line no absolute)
+    relativenumber = true, -- Current line actual line number, other relative to cursor
+    scrolloff = 3, -- Lines to keep on screen
+    shiftwidth = 0, -- Spaces to use for autoindent. 0 uses tabstop value
+    showbreak = "↪", -- Indicator at the start of wrapped lines
+    showmatch = true, -- Highlight matching brackets as you type
+    sidescrolloff = 3, -- Min cols to keep on screen
+    smartcase = true, -- ...unless search includes uppercase letters...
+    softtabstop = -1, -- If negative, shiftwidth value is used
+    spell = true, -- Spellcheck by default
+    splitbelow = true, splitright = true, -- New windows below and to right
+    swapfile = false, -- How often does vim crash anyway?
+    tabstop = 4, -- Spaces a tab is worth
+    termguicolors = true, -- Enable colors in the terminal
+    timeoutlen = 500, -- MS to wait for mapping to complete
+    wildignorecase = true, -- Case insensitive completion
+    wrap = true, -- Wrap long lines, but not in the middle of a word, and maintain indent level
+    backup = false, -- Don't need backups with infinite undo
+    undodir = os.getenv("HOME") .. "/.vim/undodir", -- Save undo history
+    undofile = true, -- Save undo history
+
 }) do vim.opt[key] = value end
 
 for key, value in pairs({ -- globals (for older plugins)
-	mapleader = ' ',
-	maplocalleader = ' ',
-	t_co = 256,
 
-	-- Netrw plugin options
-	netrw_liststyle = 3,
-	netrw_browse_split = 4,
-	netrw_winsize = 25,
-	netrw_banner = 0,
-	-- netrw_altv = 1,
+    mapleader = ' ',
+    maplocalleader = ' ',
+    t_co = 256,
+
+    -- Netrw plugin options
+    netrw_liststyle = 3,
+    netrw_browse_split = 4,
+    netrw_winsize = 25,
+    netrw_banner = 0,
+    -- netrw_altv = 1,
+
 }) do vim.g[key] = value end
 -- vim: foldlevel=1
