be0912fb8ac7d104628d4add7749d62ce8036f37
Josh Surber <josh@surber.us>
2023-01-09 18:33:07 -0600

    nvim: Move autocmds and req statements to user/init.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,1 @@
-vim.g.mapleader = " "
-
-require("user.autocmds") -- Do stuff when stuff happens
-require("user.mappings") -- Key mappings
-require("user.plugins") -- Packer install stuff
-require("user.settings") -- Basic options
-
--- vim: foldlevel=2
+require('user')
---
+++ lua/user/init.lua
@@ -0,0 +1,57 @@
+require('user.set')
+require('user.map')
+require('user.packer')
+
+local A = vim.api
+
+-- Remove useless stuff from the terminal window and enter INSERT mode
+A.nvim_create_autocmd('TermOpen', {
+    callback = function(data)
+        if not string.find(vim.bo[data.buf].filetype, '^[fF][tT]erm') then
+            A.nvim_set_option_value('number', false, { scope = 'local' })
+            A.nvim_set_option_value('relativenumber', false, { scope = 'local' })
+            A.nvim_set_option_value('signcolumn', 'no', { scope = 'local' })
+            A.nvim_command('startinsert')
+        end
+    end,
+})
+
+-- Help window on right, navigate helptags with enter and backspace
+A.nvim_create_autocmd('FileType', {
+    pattern = "help",
+    callback = function()
+        vim.cmd "wincmd L"
+        vim.keymap.set('n', '<CR>', '<C-]>', { noremap = true, silent = true, buffer = true })
+        vim.keymap.set('n', '<BS>', '<C-T>', { noremap = true, silent = true, buffer = true })
+    end
+})
+
+-- Use relativenumber in current window only
+local numbertoggle = A.nvim_create_augroup('numbertoggle', { clear = true })
+A.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave" }, {
+    group = numbertoggle,
+    callback = function()
+        A.nvim_set_option_value('relativenumber', true, { scope = 'local' })
+    end
+})
+A.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter" }, {
+    group = numbertoggle,
+    callback = function()
+        A.nvim_set_option_value('relativenumber', false, { scope = 'local' })
+    end
+})
+
+-- Do not use smart case in command line mode
+local dynamic_smartcase = A.nvim_create_augroup('dynamic_smartcase', { clear = true })
+A.nvim_create_autocmd('CmdLineEnter', {
+    group = dynamic_smartcase,
+    callback = function()
+        A.nvim_set_option('smartcase', false)
+    end
+})
+A.nvim_create_autocmd('CmdLineLeave', {
+    group = dynamic_smartcase,
+    callback = function()
+        A.nvim_set_option('smartcase', true)
+    end
+})
