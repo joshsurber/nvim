4061a9537989f0a45eefea014b0d624a3c457922
Josh Surber <josh@surber.us>
2023-01-18 15:50:14 -0600

    nvim: Don't reset fold behaviour all the damn time

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -105,13 +105,13 @@ if not vim.wo.diff then
     vim.opt.foldexpr   = 'nvim_treesitter#foldexpr()'
 end
 
----WORKAROUND for folding
-vim.api.nvim_create_autocmd({ 'BufEnter', 'BufAdd', 'BufNew', 'BufNewFile', 'BufWinEnter' }, {
-    group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
-    callback = function()
-        if vim.wo.diff then return end
-        vim.opt.foldmethod = 'expr'
-        vim.opt.foldexpr   = 'nvim_treesitter#foldexpr()'
-    end
-})
----ENDWORKAROUND
+-- ---WORKAROUND for folding
+-- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufAdd', 'BufNew', 'BufNewFile', 'BufWinEnter' }, {
+--     group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+--     callback = function()
+--         if vim.wo.diff then return end
+--         vim.opt.foldmethod = 'expr'
+--         vim.opt.foldexpr   = 'nvim_treesitter#foldexpr()'
+--     end
+-- })
+-- ---ENDWORKAROUND
