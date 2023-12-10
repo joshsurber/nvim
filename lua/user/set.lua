f0ece931ec2b84b90667db6be2a2808c92482ef4
Josh Surber <josh@surber.us>
2023-12-10 07:14:12 -0600

    nvim: settings for neovide

--- lua/user/set.lua
+++ lua/user/set.lua
@@ -4,7 +4,7 @@ for key, value in pairs({ -- NeoVim settings
     confirm = true, -- Instead of failing a command because of unsaved changes, raise a dialogue asking if you wish to save changed files.
     dictionary = "/usr/share/dict/words",
     expandtab = true, -- Use spaces instead of tabs
-    guifont='CaskaydiaCove NF:h10', -- For Neovide etc
+    guifont = 'CaskaydiaCove NF:h10', -- For Neovide etc
     keywordprg = ':help', -- 'K' searches vim docs
     lazyredraw = true, -- Don't redraw screen while executing macros
     scrolloff = 3, -- Lines to keep on screen
@@ -21,6 +21,15 @@ for key, value in pairs({ -- NeoVim settings
 
 }) do vim.opt[key] = value end
 
+-- -- Helper function for transparency formatting
+-- local alpha = function()
+--   return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+-- end
+-- -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+-- vim.g.neovide_transparency = 0.8
+-- vim.g.transparency = 0.8
+-- vim.g.neovide_background_color = "#0f1117" .. alpha()
+
 for key, value in pairs({ -- globals (for older plugins)
 
     mapleader = ' ',
@@ -28,6 +37,16 @@ for key, value in pairs({ -- globals (for older plugins)
     t_co = 256,
 
     nvim_markdown_preview_format = 'markdown',
+
+    neovide_floating_blur_amount_x = 2.0,
+    neovide_floating_blur_amount_y = 2.0,
+
+    neovide_floating_shadow = true,
+    neovide_floating_z_height = 10,
+    neovide_light_angle_degrees = 45,
+    neovide_light_radius = 5,
+
+    neovide_hide_mouse_when_typing = true,
 
 }) do vim.g[key] = value end
 -- vim: fdl=1
