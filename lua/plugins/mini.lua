bb8f55c04cf0d73f8cdf78637ec1e5a5e836fc28
Josh Surber <josh@surber.us>
2024-12-29 10:39:29 -0600

    nvim: change how mini.completion works

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -149,8 +149,8 @@ return {
             },
             completion = {
                 after = function()
-                    local keycode = vim.keycode or function(x)
-                        return vim.api.nvim_replace_termcodes(x, true, true, true)
+                    local imap_expr = function(lhs, rhs)
+                        vim.keymap.set('i', lhs, rhs, { expr = true })
                     end
                     local keys = {
                         ['cr']        = keycode('<CR>'),
@@ -158,20 +158,32 @@ return {
                         ['ctrl-y_cr'] = keycode('<C-y><CR>'),
                     }
 
-                    _G.cr_action = function()
-                        if vim.fn.pumvisible() ~= 0 then
-                            -- If popup is visible, confirm selected item or add new line otherwise
-                            local item_selected = vim.fn.complete_info()['selected'] ~= -1
-                            return item_selected and keys['ctrl-y'] or keys['ctrl-y_cr']
-                        else
-                            -- If popup is not visible, use plain `<CR>`. You might want to customize
-                            -- according to other plugins. For example, to use 'mini.pairs', replace
-                            -- next line with `return require('mini.pairs').cr()`
-                            return keys['cr']
-                        end
-                    end
+                    -- local keycode = vim.keycode or function(x)
+                    --     return vim.api.nvim_replace_termcodes(x, true, true, true)
+                    -- end
+                    -- local keys = {
+                    --     ['cr']        = keycode('<CR>'),
+                    --     ['ctrl-y']    = keycode('<C-y>'),
+                    --     ['ctrl-y_cr'] = keycode('<C-y><CR>'),
+                    -- }
 
-                    vim.keymap.set('i', '<CR>', 'v:lua._G.cr_action()', { expr = true })
+                    imap_expr('<Tab>', [[pumvisible() ? "\<C-n>" : "\<Tab>"]])
+                    imap_expr('<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]])
+
+                    -- _G.cr_action = function()
+                    --     if vim.fn.pumvisible() ~= 0 then
+                    --         -- If popup is visible, confirm selected item or add new line otherwise
+                    --         local item_selected = vim.fn.complete_info()['selected'] ~= -1
+                    --         return item_selected and keys['ctrl-y'] or keys['ctrl-y_cr']
+                    --     else
+                    --         -- If popup is not visible, use plain `<CR>`. You might want to customize
+                    --         -- according to other plugins. For example, to use 'mini.pairs', replace
+                    --         -- next line with `return require('mini.pairs').cr()`
+                    --         return keys['cr']
+                    --     end
+                    -- end
+
+                    -- vim.keymap.set('i', '<CR>', 'v:lua._G.cr_action()', { expr = true })
                 end
             },
             hipatterns = {
