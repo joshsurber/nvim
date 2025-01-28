3b3d48f180e82e6a56798aeaab1061d5a970cd86
Josh Surber <josh@surber.us>
2025-01-28 08:12:21 -0600

    nvim: lazygit bindings

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -16,6 +16,22 @@ return {
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+        { "<leader>gl", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+    after = function()
+        vim.keymap.set("n", "<leader>lg", function()
+            --  get file name with extension
+            local file = vim.fn.expand("%:t")
+            vim.cmd("LazyGit")
+
+            -- Wait a bit for LazyGit to load
+            vim.defer_fn(function()
+                -- search for the file, highlight, and exit search mode in lazygit
+                vim.api.nvim_feedkeys("/" .. file, "t", true)
+                vim.api.nvim_input("<CR>")
+                vim.api.nvim_input("<ESC>")
+            end, 150) -- (milliseconds)
+        end, { desc = "[g]it" })
+    end
 }
