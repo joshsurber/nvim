a69ea033a912227af08133a801bb1852f46e3ed9
Josh Surber <josh@surber.us>
2025-03-04 19:40:31 -0600

    Correct cloning public repos

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,5 +1,10 @@
-require('mini.deps').add('nvimtools/none-ls.nvim')
-local null_ls = require('null-ls')
+require("mini.deps").add({
+	source = "nvimtools/none-ls.nvim",
+	depends = {
+		"jay-babu/mason-null-ls.nvim",
+	},
+})
+local null_ls = require("null-ls")
 
 local code_action = null_ls.builtins.code_actions
 local completion = null_ls.builtins.completion
@@ -7,22 +12,19 @@ local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
 local hover = null_ls.builtins.hover
 
-null_ls.setup({
-    sources = {
-        -- fmt.stylua,
-        -- formatting.tidy,
-        formatting.prettier.with({ extra_filetypes = { "liquid" }, }),
-        diagnostics.tidy,
-        -- formatting.markdownlint,
-        -- formatting.mdformat,
-        formatting.shfmt.with({ extra_args = { "-i", "4" } }),
-    },
-})
-return {
-    -- { 'jose-elias-alvarez/null-ls.nvim',
-    {
-        'nvimtools/none-ls.nvim',
-        config = function()
-        end
-    },
-} -- Inject 3rd party executables into LSP -- https://github.com/jose-elias-alvarez/null-ls.nvim
+-- null_ls.setup({
+-- 	sources = {
+-- 		-- fmt.stylua,
+-- 		-- formatting.tidy,
+-- 		formatting.prettier.with({ extra_filetypes = { "liquid" } }),
+-- 		diagnostics.tidy,
+-- 		-- formatting.markdownlint,
+-- 		-- formatting.mdformat,
+-- 		formatting.shfmt.with({ extra_args = { "-i", "4" } }),
+-- 	},
+-- })
+
+-- require("mason-null-ls").setup({
+-- 	handlers = {},
+-- 	automatic_installation = true,
+-- })
