7b8d24245f0d25f70247d241861520b7a1f9010a
Josh Surber <josh@surber.us>
2025-03-06 10:17:44 -0600

    Move arrays into functions

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,14 +1,4 @@
-local PluginFiles = {
-    "lsp",
-    "cmp",
-    "treesitter",
-    'null-ls',
-    "mini",
-    "colorschemes",
-    -- "lazygit",
-}
-
-local Plugins = {
+for _, repo in pairs({
     "folke/tokyonight.nvim",
     "ellisonleao/gruvbox.nvim",
     "github/copilot.vim",
@@ -16,14 +6,12 @@ local Plugins = {
     "alexghergh/nvim-tmux-navigation",
     "kdheepak/lazygit.nvim",
     -- "nvim-lua/plenary.nvim",
-
     { -- cmp
         source = "hrsh7th/nvim-cmp",
         depends = {
             "hrsh7th/cmp-nvim-lsp",
             "abeldekat/cmp-mini-snippets" },
     },
-
     { -- lsp
         source = "neovim/nvim-lspconfig",
         depends = {
@@ -31,7 +19,6 @@ local Plugins = {
             "olrtg/nvim-emmet",
         },
     },
-
     { -- mason
         source = "williamboman/mason.nvim",
         depends = {
@@ -39,14 +26,12 @@ local Plugins = {
             -- "mhartington/formatter.nvim",
         },
     },
-
     { -- null-ls
         source = "nvimtools/none-ls.nvim",
         depends = {
             "jay-babu/mason-null-ls.nvim",
         },
     },
-
     { -- treesitter
         source = "nvim-treesitter/nvim-treesitter",
         hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
@@ -56,35 +41,17 @@ local Plugins = {
             'HiPhish/rainbow-delimiters.nvim',
         },
     },
-}
+}) do require("mini.deps").add(repo) end
 
--- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
-local path_package = vim.fn.stdpath("data") .. "/site/"
-local mini_path = path_package .. "pack/deps/start/mini.nvim"
-if not vim.loop.fs_stat(mini_path) then
-    vim.cmd('echo "Installing `mini.nvim`" | redraw')
-    local clone_cmd = {
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/echasnovski/mini.nvim",
-        mini_path,
-    }
-    vim.fn.system(clone_cmd)
-    vim.cmd("packadd mini.nvim | helptags ALL")
-    vim.cmd('echo "Installed `mini.nvim`" | redraw')
-end
-
--- Set up 'mini.deps' (customize to your liking)
-require("mini.deps").setup({ path = { package = path_package } })
-
-for _, repo in pairs(Plugins) do
-    require("mini.deps").add(repo)
-end
-
-for _, plugin in pairs(PluginFiles) do
-    require("plugins/" .. plugin)
-end
+for _, plugin in pairs({
+    "lsp",
+    "cmp",
+    "treesitter",
+    'null-ls',
+    "mini",
+    "colorschemes",
+    "lazygit",
+}) do require("plugins/" .. plugin) end
 
 require("toggleterm").setup({
     open_mapping = "<C-g>",
