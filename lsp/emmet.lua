cf28de671c91baf2b77604e80e5230e35131e84d
Josh Surber <josh@surber.us>
2025-07-10 14:26:54 -0500

    LSP settings

---
+++ lsp/emmet.lua
@@ -0,0 +1,5 @@
+return {
+    cmd = { 'emmet-language-server' },
+    filetypes = { "astro", "css", "eruby", "html", "javascript",
+        "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
+}
