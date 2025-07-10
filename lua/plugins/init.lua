cf10e93aba01f96175fae6b1a04c9300a5e1a47b
Josh Surber <josh@surber.us>
2025-07-10 16:42:53 -0500

    Split off lsp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,6 +8,7 @@ require("plugins/mini")
 -- require("plugins/toggleterm")
 require("plugins/colorschemes")
 require("plugins/lazygit")
+require("plugins/lsp")
 require("plugins/treesitter")
 
 add("github/copilot.vim")
