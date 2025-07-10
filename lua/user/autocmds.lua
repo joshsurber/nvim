aa6e8b4f1d43f952409ed517f84fb24f97aa6838
Josh Surber <josh@surber.us>
2025-07-10 14:27:06 -0500

    Move autocmds to their own file

---
+++ lua/user/autocmds.lua
@@ -0,0 +1,19 @@
+
+local A = vim.api
+
+-- Change CWD to current file's directory
+A.nvim_create_user_command('CD', function()
+    vim.cmd.cd(
+        vim.fn.expand('%:h')
+    )
+end, {})
+
+-- Help window on right, navigate helptags with enter and backspace
+A.nvim_create_autocmd('FileType', {
+    pattern = "help",
+    callback = function()
+        vim.cmd "wincmd L"
+        vim.keymap.set('n', '<CR>', '<C-]>', { noremap = true, silent = true, buffer = true })
+        vim.keymap.set('n', '<BS>', '<C-T>', { noremap = true, silent = true, buffer = true })
+    end
+})
