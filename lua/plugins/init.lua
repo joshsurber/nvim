d533142f96e91861187b6ca24da423d8b28950a6
Josh Surber <josh@surber.us>
2025-07-10 16:41:47 -0500

    Split off lazygit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,28 +5,7 @@ require("plugins/lsp")
 require("plugins/treesitter")
 -- require("plugins/null-ls")
 require("plugins/mini")
-require("plugins/lazygit")
 require("plugins/colorschemes")
+require("plugins/lazygit")
 
 add("github/copilot.vim")
-add("kdheepak/lazygit.nvim")
-
---     shade_terminals = true,
--- add("akinsho/toggleterm.nvim")
--- require("toggleterm").setup({
---     open_mapping = "<C-g>",
---     direction = "horizontal",
--- })
-
-add("alexghergh/nvim-tmux-navigation")
-require "nvim-tmux-navigation".setup({
-    disable_when_zoomed = true, -- defaults to false
-    keybindings = {
-        left        = "<C-h>",
-        down        = "<C-n>",
-        up          = "<C-e>",
-        right       = "<C-i>",
-        last_active = "<C-\\>",
-        next        = "<C-Space>",
-    }
-})
