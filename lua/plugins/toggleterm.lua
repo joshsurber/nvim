67909dc43eeb5adad117b7aa6d121120d5e30bde
Josh Surber <josh@surber.us>
2025-07-10 16:42:39 -0500

    Split off toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,7 @@
+local add = require("mini.deps").add
+add("akinsho/toggleterm.nvim")
+require("toggleterm").setup({
+    shade_terminals = true,
+    open_mapping = "<C-g>",
+    direction = "horizontal",
+})
