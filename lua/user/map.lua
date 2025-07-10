c2c146f7c21ffc74782808b677431e0f4c8e39d0
Josh Surber <josh@surber.us>
2025-07-10 14:23:44 -0500

    Mapping tweaks

--- lua/user/map.lua
+++ lua/user/map.lua
@@ -1,9 +1,9 @@
 local map = vim.keymap.set
 
--- Easy access to edit init.lua
-map("n", "<leader>ve", ":tabedit $MYVIMRC<cr>", { desc = 'Edit init.lua' })
-map("n", "<leader>vs", ":source $MYVIMRC<cr>", { desc = 'Reload init.lua' })
-map("n", "<leader>vd", ":cd ~/.config/nvim<cr>", { desc = 'Change to Neovim config directory' })
+-- -- Easy access to edit init.lua
+-- map("n", "<leader>ve", ":tabedit $MYVIMRC<cr>", { desc = 'Edit init.lua' })
+-- map("n", "<leader>vs", ":source $MYVIMRC<cr>", { desc = 'Reload init.lua' })
+-- map("n", "<leader>vd", ":cd ~/.config/nvim<cr>", { desc = 'Change to Neovim config directory' })
 
 
 -- Command line conveniences
@@ -11,7 +11,7 @@ map("c", "%%", "<C-R>=expand('%:h').'/'<cr>") -- expand %% to current directory 
 -- THIS DOESN'T WORK ANYMORE ANYWAY --
 map("c", "w!!", "w !sudo tee % >/dev/null")   -- No write permission? Fuck you, do it anyway!")
 map('t', '<C-w>', '<C-\\><C-n><C-w>')
-map('t', '<tab>', '<tab>')                    -- for some reason w/o this tab completion doesn't work
+-- map('t', '<tab>', '<tab>')                    -- for some reason w/o this tab completion doesn't work
 -- Folding
 map({ 'n', 'v' }, "<leader><leader>", "za", { desc = 'Toggle fold' })
 
@@ -22,13 +22,13 @@ map("n", '*', '*N', { desc = 'Search word under cursor' }) -- Fix * (Keep the cu
 map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace word under cursor" })
 map("n", "<esc>", "<cmd>noh<cr><cmd>echo<cr>") --  remove search highlighting
 
--- Fix oddities with visual selections
--- Fix linewise visual selection of various text objects
-map("n", "VV", "V", { desc = "Select visual lines" })
-map("n", "Vit", "vitVkoj", { desc = "Select inside a tag" })
-map("n", "Vat", "vatV", { desc = "Select around a tag" })
-map("n", "Vab", "vabV", { desc = "Select a block" })
-map("n", "VaB", "vaBV", { desc = "Select a block" })
+-- -- Fix oddities with visual selections
+-- -- Fix linewise visual selection of various text objects
+-- map("n", "VV", "V", { desc = "Select visual lines" })
+-- map("n", "Vit", "vitVkoj", { desc = "Select inside a tag" })
+-- map("n", "Vat", "vatV", { desc = "Select around a tag" })
+-- map("n", "Vab", "vabV", { desc = "Select a block" })
+-- map("n", "VaB", "vaBV", { desc = "Select a block" })
 
 -- Go to end of visual selection
 map('v', 'y', 'y`]')
@@ -36,14 +36,14 @@ map('v', 'p', 'p`]')
 map('n', 'p', 'p`]')
 
 -- Change modes easier
--- map("i", "jk", "<esc>")
--- map("t", "<Esc>", "<C-\\><C-n>")
+map("i", "nene", "<esc>")
+map("t", "<Esc>", "<C-\\><C-n>")
 
 -- Save and quit easier
 map("n", "<leader>w", "<cmd>w<cr>", { desc = 'Save' })
 map("n", "<leader>W", "<cmd>wall<cr>", { desc = 'Save all windows/tabs' })
 map("n", "<leader>q", "<cmd>bdelete<cr>", { desc = 'Delete buffer' })
-map("n", "<leader>Q", "<cmd>qall<cr>", { desc = 'Close all windows/tabs' })
+-- map("n", "<leader>Q", "<cmd>qall<cr>", { desc = 'Close all windows/tabs' })
 -- map("n", "QQ", ":q<cr>")
 
 map("n", '<C-q>', ':q<cr>') -- Access `qq` macro
