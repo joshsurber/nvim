02c00b59cbde2fb4fdfd627ecf615492d8a16075
Josh Surber <josh@surber.us>
2025-07-16 09:17:33 -0500

    Disable lazygit plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,11 +2,11 @@ local add = require("mini.deps").add
 
 -- require("plugins/cmp")
 -- require("plugins/formatter")
+-- require("plugins/lazygit")
 -- require("plugins/null-ls")
 -- require("plugins/toggleterm")
 add("github/copilot.vim")
 require("plugins/colorschemes")
-require("plugins/lazygit")
 require("plugins/lsp")
 require("plugins/mini")
 require("plugins/tmux")
