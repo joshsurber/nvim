413a4daab2b53b090d0d5dec794b3ee6e0401729
Josh Surber <josh@surber.us>
2025-07-18 00:28:36 -0500

    Remove format on save autocmd
    Format on save plugin does thes

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -33,15 +33,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
             vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
         end
 
-        if not client:supports_method('textDocument/willSaveWaitUntil')
-            and client:supports_method('textDocument/formatting') then
-            vim.api.nvim_create_autocmd('BufWritePre', {
-                group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
-                buffer = event.buf,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = event.buf, id = client.id, timeout_ms = 1000 })
-                end,
-            })
-        end
+        -- if not client:supports_method('textDocument/willSaveWaitUntil')
+        --     and client:supports_method('textDocument/formatting') then
+        --     vim.api.nvim_create_autocmd('BufWritePre', {
+        --         group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+        --         buffer = event.buf,
+        --         callback = function()
+        --             vim.lsp.buf.format({ bufnr = event.buf, id = client.id, timeout_ms = 1000 })
+        --         end,
+        --     })
+        -- end
     end,
 })
