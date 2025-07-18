636aa78f086b192c134faa288348e555fb7377bf
Josh Surber <josh@surber.us>
2025-07-18 00:25:44 -0500

    Remove lsp directory
    Using lspconfig to make this simpler

--- lsp/emmet.lua
+++
@@ -1,5 +0,0 @@
-return {
-    cmd = { 'emmet-language-server' },
-    filetypes = { "astro", "css", "eruby", "html", "javascript",
-        "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
-}
--- lsp/lua.lua
+++
@@ -1,9 +0,0 @@
-return {
-    cmd = { 'lua-language-server' },
-    filetypes = { 'lua' },
-    root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-    settings = {
-        Lua = { diagnostics = { globals = { "vim" } } },
-        workspace = { library = { vim.env.VIMRUNTIME, }, },
-    }
-}
