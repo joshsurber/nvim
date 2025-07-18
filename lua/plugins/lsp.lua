adde6c3f4424dfbe91350fc8f17bed1f12bff06b
Josh Surber <josh@surber.us>
2025-07-18 00:27:47 -0500

    Run through stylua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,33 +5,47 @@ add("williamboman/mason-lspconfig.nvim")
 add("olrtg/nvim-emmet")
 -- add("rafamadriz/friendly-snippets")
 
-vim.lsp.config('lua_ls', {
-    settings = {
-        Lua = { diagnostics = { globals = { "vim" } } },
-        runtime = { version = 'LuaJIT', },
-        workspace = { library = { vim.env.VIMRUNTIME, }, },
-    }
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = { diagnostics = { globals = { "vim" } } },
+		runtime = { version = "LuaJIT" },
+		workspace = { library = { vim.env.VIMRUNTIME } },
+	},
+})
+vim.lsp.config("emmet_language_server", {
+	filetypes = {
+		"astro",
+		"css",
+		"html",
+		"javascript",
+		"javascriptreact",
+		"less",
+		"sass",
+		"scss",
+		"typescriptreact",
+	},
 })
 
-vim.lsp.config('emmet_language_server', {
-    filetypes = { "astro", "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
-})
-vim.keymap.set({ "n", "v" }, '<leader>h', require('nvim-emmet').wrap_with_abbreviation)
+vim.keymap.set(
+	{ "n", "v" },
+	"<leader>h",
+	require("nvim-emmet").wrap_with_abbreviation,
+	{ desc = "Wrap with emmet abbreviation" }
+)
 
 require("mason").setup({})
 require("mason-lspconfig").setup({})
 
-vim.opt.winborder = 'rounded'
+vim.opt.winborder = "rounded"
 vim.diagnostic.config({
-    virtual_text = true,
-    virtual_lines = { current_line = true },
-    signs = {
-        text = {
-            [vim.diagnostic.severity.ERROR] = '✘',
-            [vim.diagnostic.severity.WARN] = '▲',
-            [vim.diagnostic.severity.HINT] = '⚑',
-            [vim.diagnostic.severity.INFO] = ''
-        },
-    },
+	virtual_text = true,
+	virtual_lines = { current_line = true },
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "✘",
+			[vim.diagnostic.severity.WARN] = "▲",
+			[vim.diagnostic.severity.HINT] = "⚑",
+			[vim.diagnostic.severity.INFO] = "",
+		},
+	},
 })
-vim.cmd([[ command! Format :lua vim.lsp.buf.format() ]])
