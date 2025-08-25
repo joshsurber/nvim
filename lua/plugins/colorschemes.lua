b77c28d8c22ba5a95c0494ecfa82e2f8492be327
Josh Surber <josh@surber.us>
2025-08-25 08:42:50 -0500

    "Blockify" colorscheme file
    This allows changing colorscheme by commenting/uncommenting folded
    blocks

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,8 +1,8 @@
 local add = require("mini.deps").add
-add("folke/tokyonight.nvim")
-add("ellisonleao/gruvbox.nvim")
-add("rose-pine/neovim")
 
+-- add("ellisonleao/gruvbox.nvim")-- {{{
+-- vim.cmd.colorscheme("gruvbox")-- }}}
+add("folke/tokyonight.nvim") -- {{{
 require("tokyonight").setup({
     transparent = true, -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
@@ -21,37 +21,39 @@ require("tokyonight").setup({
     dim_inactive = true, -- dims inactive windows
     lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
 })
-require("rose-pine").setup({
-    variant = "auto", -- auto, main, moon, or dawn
-    dark_variant = "main", -- main, moon, or dawn
-    dim_inactive_windows = true,
-    extend_background_behind_borders = true,
+vim.cmd.colorscheme("tokyonight") -- }}}
+-- add("rose-pine/neovim") -- {{{
+-- require("rose-pine").setup({
+--     variant = "auto", -- auto, main, moon, or dawn
+--     dark_variant = "main", -- main, moon, or dawn
+--     dim_inactive_windows = true,
+--     extend_background_behind_borders = true,
+--
+--     styles = {
+--         bold = true,
+--         italic = true,
+--         transparency = true,
+--     },
+--
+--     highlight_groups = {
+--         -- Comment = { fg = "foam" },
+--         -- StatusLine = { fg = "love", bg = "love", blend = 15 },
+--         -- VertSplit = { fg = "muted", bg = "muted" },
+--         -- Visual = { fg = "base", bg = "text", inherit = false },
+--     },
+--
+--     before_highlight = function(group, highlight, palette)
+--         -- Disable all undercurls
+--         -- if highlight.undercurl then
+--         --     highlight.undercurl = false
+--         -- end
+--         --
+--         -- Change palette colour
+--         -- if highlight.fg == palette.pine then
+--         --     highlight.fg = palette.foam
+--         -- end
+--     end,
+-- })
+-- vim.cmd.colorscheme("rose-pine") -- }}}
 
-    styles = {
-        bold = true,
-        italic = true,
-        transparency = true,
-    },
-
-    highlight_groups = {
-        -- Comment = { fg = "foam" },
-        -- StatusLine = { fg = "love", bg = "love", blend = 15 },
-        -- VertSplit = { fg = "muted", bg = "muted" },
-        -- Visual = { fg = "base", bg = "text", inherit = false },
-    },
-
-    before_highlight = function(group, highlight, palette)
-        -- Disable all undercurls
-        -- if highlight.undercurl then
-        --     highlight.undercurl = false
-        -- end
-        --
-        -- Change palette colour
-        -- if highlight.fg == palette.pine then
-        --     highlight.fg = palette.foam
-        -- end
-    end,
-})
-
-vim.cmd.colorscheme("tokyonight")
--- vim.cmd.colorscheme("rose-pine")
+-- vim: fdm=marker fdl=0
