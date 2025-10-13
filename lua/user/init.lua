85700ad799c939085c02ae20450273a112f09fe1
Josh Surber <josh@surber.us>
2025-10-13 18:24:32 -0500

    Added command to close all but current buffer

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -2,3 +2,8 @@ require("user.set")
 require("user.map")
 require("user.colemak")
 require("user.autocmds")
+
+vim.api.nvim_create_user_command("Bonly", function()
+    -- vim.cmd([[%bdelete|edit #|silent normal ` "]])
+    vim.cmd([[%bdelete|edit #]])
+end, {})
