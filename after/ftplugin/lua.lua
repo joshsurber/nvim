0198db76c1cce5eaa1b3cd77d3f0550ef108fc70
Josh Surber <josh@surber.us>
2025-10-14 10:05:00 -0500

    Modify lua_ls config

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,4 +1,4 @@
-vim.opt_local.suffixesadd:prepend('.lua')
-vim.opt_local.suffixesadd:prepend('init.lua')
-vim.opt_local.path:prepend(vim.fn.stdpath('config') .. '/lua')
-vim.opt_local.path:prepend(vim.fn.stdpath('config') .. '/lua/user')
+-- vim.opt_local.suffixesadd:prepend('.lua')
+-- vim.opt_local.suffixesadd:prepend('init.lua')
+-- vim.opt_local.path:prepend(vim.fn.stdpath('config') .. '/lua')
+-- vim.opt_local.path:prepend(vim.fn.stdpath('config') .. '/lua/user')
